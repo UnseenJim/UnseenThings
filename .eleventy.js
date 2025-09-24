f7c672f765f9e03e62f8177a53d772cb128fb8c0
James Carney <james@unseenthings.co.uk>
2025-09-24 17:19:51 +0100

    Update layout and content with images and styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
- 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+
   return {
     dir: {
       input: ".",
