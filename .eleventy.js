0cb639bd7c2ae4bea6dd268aa0ff3e8f2357fc3f
UnseenJim <james@unseenthings.co.uk>
2025-10-16 21:06:58 +0100

    Add favicon passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("Logo.svg");
+  eleventyConfig.addPassthroughCopy("Logo32.png");
+  eleventyConfig.addPassthroughCopy("Logo16.png");
+  eleventyConfig.addPassthroughCopy("Logo180.png"); // if you use it for Apple touch
+
 
   return {
     dir: {
